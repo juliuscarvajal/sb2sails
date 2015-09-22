71a29811f2d35574b4ccdc06d4c183bd44d1d613
Julius Carvajal <juliuscarvajal21@gmail.com>
2015-09-22 10:59:18 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb2sails
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/Locations.js
@@ -0,0 +1,18 @@
+/**
+ * Locations.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+  tableName: 'location',
+  attributes: {
+    name: {
+      type: 'string'
+    },
+    location: {
+      type: 'string'
+    }
+  }
+};
